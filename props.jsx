4dbd54feca408d487b6856fb9f7d8f982dfc0c8f
Ravidhote17 <ravidhote101@gmail.com>
2024-09-09 17:34:07 +0530

    props

--- props.jsx
+++ props.jsx
@@ -42,29 +42,28 @@ import Card from "./components/Card"
 
 function App(){
 
-  const data = [
+  const raw = [
     {name:"Ravi", professsion:"Painter", image:"https://plus.unsplash.com/premium_photo-1669882305273-674eff6567af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG90cmFpdHxlbnwwfHwwfHx8MA%3D%3D", freinds: false},
     {name:"Ritik", professsion:"Carpainter", image:"https://images.unsplash.com/photo-1519713958759-6254243c4a53?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG90cmFpdHxlbnwwfHwwfHx8MA%3D%3D", freinds: false},
     {name:"ujjwal", professsion:"Teacher", image:"https://images.unsplash.com/photo-1526306525210-b5094a17e223?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cG90cmFpdHxlbnwwfHwwfHx8MA%3D%3D", freinds: false},
     {name:"vishal", professsion:"Driver", image:"https://plus.unsplash.com/premium_photo-1664451177475-a8a9b2fc3aa4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cG90cmFpdHxlbnwwfHwwfHx8MA%3D%3D", freinds: false}
   ]
 
-  const [realData, setRealData] = useState(data)
-  const handleFriendsButton = (cardindex)=>{
-    setRealData((previous)=>{
-      return previous.map((item, index)=>{
-        if(index === cardindex){
-          return {...item, freinds: !item.freinds}
-        }
+ const [data, setData] = useState(raw)
+ const handleClick = (changingIndex)=>{
+  setData((prev)=>{
+    return prev.map((item, index)=>{
+      if(index === changingIndex) return {...item, freinds: !item.freinds}
         return item
-      })
     })
-  }
+  })
+ }
+  
 
   return (
     <div className="w-full h-screen bg-zinc-300 gap-4 flex items-center justify-center">
-      {realData.map((item, index)=>
-        <Card key={index} index={index} name={item.name} image={item.image} professsion={item.professsion} handleClick = {handleFriendsButton} freinds={item.freinds}/>
+      {data.map((item, index)=>
+        <Card key={index} index={index} name={item.name} image={item.image} professsion={item.professsion} handleClick={handleClick} />
       )}
     </div>
   )
@@ -77,7 +76,7 @@ export default App;
 //----------------- CARD.JSX
 import React from 'react';
  
-function Card({image, name, professsion, handleClick, freinds, index}) {
+function Card({image, name, professsion, handleClick, freinds}) {
     
     return( 
         <div className='w-52 bg-white rounded-md overflow-hidden'>
@@ -87,7 +86,7 @@ function Card({image, name, professsion, handleClick, freinds, index}) {
             <div className='w-full p-3'>
                 <h3 className='text-xl font-semibold'>{name}</h3>
                 <h5 className='text-xs'>{professsion}</h5>
-                <button onClick={()=>handleClick(index)} className='px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded'>{freinds === true ? "Freind" : "Add Freind"}</button>
+                <button onClick={()=>handleClick(index)} className='px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded'>{freinds === true ? "Freiend" : "Add Freinds"}</button>
             </div>
         </div>
     )
